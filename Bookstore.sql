91bc74a928643eca1611fe8b115576287f539ecb
Alex <alexkiplangat22@gmail.com>
2025-04-11 15:51:48 +0300

    changes

---
+++ Bookstore.sql
@@ -0,0 +1,8 @@
+-- first line: -- This script creates a database named BookstoreDB and a table named Books within it.
+
+Create Database BookstoreDB;
+
+Use BookstoreDB;
+
+-- create tables for Book
+
