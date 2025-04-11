74aeee80731bb061843e99411720c8b2d824250e
Alex <alexkiplangat22@gmail.com>
2025-04-11 21:02:55 +0300

    changes

--- Bookstore.sql
+++ Bookstore.sql
@@ -1,11 +1,29 @@
--- first line: -- This script creates a database named BookstoreDB and a table named Books within it.
+CREATE DATABASE IF NOT EXISTS BookstoreDB;
+USE BookstoreDB;
 
-Create Database BookstoreDB;
 
-Use BookstoreDB;
+-- Book Language Table
+CREATE TABLE book_language (
+    language_id INT AUTO_INCREMENT PRIMARY KEY,
+    language_name VARCHAR(50) NOT NULL
+);
 
--- this one creates tables for Book
+-- Publisher Table
+CREATE TABLE publisher (
+    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
+    publisher_name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    phone_number VARCHAR(20)
+);
 
+-- Author Table
+CREATE TABLE author (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+
+-- create the Book table 
 CREATE TABLE book (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -17,28 +35,7 @@ CREATE TABLE book (
     FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id)
 );
 
--- this one creates tables for Book Language
-CREATE TABLE book_language (
-    language_id INT AUTO_INCREMENT PRIMARY KEY,
-    language_name VARCHAR(50) NOT NULL
-);
-
--- this one creates tables for Publisher
-CREATE TABLE publisher (
-    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
-    publisher_name VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    phone_number VARCHAR(20)
-);
-
--- this one creates tables for Author
-CREATE TABLE author (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100)
-);
-
--- this one creates tables for Book Author
+-- Book Author Join Table
 CREATE TABLE book_author (
     book_id INT,
     author_id INT,
